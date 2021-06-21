223bb354bde4c3c6719647b1b587ea06d6009570
Mgoodell97 <u0629187@utah.edu>
2021-06-21 16:55:42 -0600

    updated pf and sensor code need tune pf and info method

--- particle_filter/scripts/fake_PF_MLE_merger.py
+++ particle_filter/scripts/fake_PF_MLE_merger.py
@@ -2,11 +2,13 @@
 import rospy #include <ros/ros.h> cpp equivalent
 from enif_iuc.msg import AgentEstimatedGaussian
 
-global quadID
-global quad_agent_msg_dict
-global quad_agent_msg_dict_previous
+# global quadID
+# global quad_agent_msg_dict
+# global quad_agent_msg_dict_previous
+# global quad_agent_msg_flag
 quad_agent_msg_dict = {}
 quad_agent_msg_dict_previous = {}
+quad_agent_msg_flag = {}
 
 def agent_mle_gauss_cb(agent_mle_gauss_msg_tmp):
     quad_agent_msg_dict_previous = quad_agent_msg_dict.copy()
@@ -15,11 +17,10 @@ def agent_mle_gauss_cb(agent_mle_gauss_msg_tmp):
 
     # Update PF with new particle and renormalize the weights
     if agent_mle_gauss_msg_tmp.agent_number not in quad_agent_msg_dict_previous.keys():
-        # Add particle here
-        print "Added agent's", agent_mle_gauss_msg_tmp.agent_number, "particle"
+        quad_agent_msg_flag[agent_mle_gauss_msg_tmp.agent_number] = True # Raise flag to add particle in next loop
+
     elif quad_agent_msg_dict[agent_mle_gauss_msg_tmp.agent_number] != quad_agent_msg_dict_previous[agent_mle_gauss_msg_tmp.agent_number]:
-        # Add particle here
-        print "Added agent's", agent_mle_gauss_msg_tmp.agent_number, "particle"
+        quad_agent_msg_flag[agent_mle_gauss_msg_tmp.agent_number] = True # Raise flag to add particle in next loop
 
     # else do not update partile filter
     # just ignore repeat data
@@ -27,10 +28,9 @@ def agent_mle_gauss_cb(agent_mle_gauss_msg_tmp):
 def main():
     rospy.init_node('Fake_PF_MLE_merger') #node name is offb_node
 
-    global quadID
     quadID = rospy.get_param("quadID")               #  [-]
 
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(1)
 
     if quadID == 1:
         rospy.Subscriber("/agent_mle_gauss_data1", AgentEstimatedGaussian, agent_mle_gauss_cb)
@@ -42,7 +42,15 @@ def main():
         rospy.Subscriber("/agent_mle_gauss_data3", AgentEstimatedGaussian, agent_mle_gauss_cb)
 
     # Let ros take over
-    rospy.spin()
+    while not rospy.is_shutdown():
+
+        for key, value in quad_agent_msg_flag.items():
+            if value:
+                print "Added agent's", key, "particle"
+                print (quad_agent_msg_dict[key])
+                quad_agent_msg_flag[key] = False
+
+        rate.sleep()
 
 if __name__ == '__main__':
     try:
