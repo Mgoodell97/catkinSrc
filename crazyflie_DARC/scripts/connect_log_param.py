6c90f37aede556b50ae75534639b6b2b4d71dd50
Mgoodell97 <u0629187@utah.edu>
2021-12-06 15:00:35 -0700

    rm ros cF

---
+++ crazyflie_DARC/scripts/connect_log_param.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import logging
+import time
+import rospy
+
+import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+
+def simple_connect():
+    print("Yeah, I'm connected! :D")
+    time.sleep(3)
+    print("Now I will disconnect :'(")
+
+def main():
+
+# URI to the Crazyflie to connect to
+    #global rate = rospy.Rate()
+    uri = 'radio://0/80/2M/E7E7E7E701'
+
+    while(not rospy.is_shutdown() ):
+        cflib.crtp.init_drivers()
+        with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
+
+            simple_connect()
+
+    if __name__ == '__main__':
+        # Initialize the low-level drivers
+        try:
+            main()
+        except rospy.ROSInterruptException:
+            pass
